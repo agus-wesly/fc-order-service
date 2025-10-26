d9e9581c8a929111c5bdbdf1370262c624818ffd
Wesly <agustchannel@gmail.com>
2025-10-26 14:03:01 +0700

    feat: init fiber

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
-package main 
+package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"log"
 )
+
+type Order struct {
+	ProductId  string `json:"product_id"`
+	TotalPrice int `json:"total_price"`
+	Status     string `json:"status"`
+}
 
 func main() {
 	app := fiber.New()
-	app.Get("/", func (c *fiber.Ctx) error {
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World")
-	}) 
+	})
+
+	app.Post("/orders", func(c *fiber.Ctx) error {
+		order := new(Order)
+		if err := c.BodyParser(order); err != nil {
+			return err
+		}
+
+		return c.Status(fiber.StatusOK).JSON(order)
+	})
 
 	app.Listen(":5959")
 }
