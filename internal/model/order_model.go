e4e112aec2ce4d0f371402222caf2fc7d30e8422
Wesly <agustchannel@gmail.com>
2025-10-27 21:03:40 +0700

    fix: the order request type

--- internal/model/order_model.go
+++ internal/model/order_model.go
@@ -2,8 +2,6 @@ package model
 
 type CreateOrderRequest struct {
 	ProductId  string `json:"product_id" validate:"required,uuid"`
-	TotalPrice int64  `json:"total_price" validate:"required"`
-	Status     string `json:"status" validate:"required"`
 }
 
 type OrderResponse struct {
