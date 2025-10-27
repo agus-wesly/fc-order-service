f250173f5eda5a974991c0a05a95bc7efdd4e413
Wesly <agustchannel@gmail.com>
2025-10-27 21:26:41 +0700

    feat: error handling

--- internal/gateway/http/product_gateway.go
+++ internal/gateway/http/product_gateway.go
@@ -1,10 +1,12 @@
 package httpgateway
 
 import (
-	"order-service/internal/model"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"order-service/internal/model"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 type ProductGateway struct {
@@ -15,13 +17,20 @@ func NewProductGateway(baseURL string) *ProductGateway {
 	return &ProductGateway{baseURL: baseURL}
 }
 
-
 func (c *ProductGateway) GetProductInfo(productId string) (*model.ProductResponse, error) {
 	resp, err := http.Get(fmt.Sprintf("%s/products/%s", c.baseURL, productId))
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == 404 {
+			return nil, fiber.NewError(fiber.StatusBadRequest, "Product not found")
+		}
+
+		return nil, fiber.ErrInternalServerError
+	}
 
 	var result model.ProductResponse
 	if err = json.NewDecoder(resp.Body).Decode(&result); err != nil {
