c90daa9f5bd0ffc0dd5cdf1bd27326ddb7480e72
Wesly <agustchannel@gmail.com>
2025-10-29 18:58:48 +0700

    feat: finishing unit test

--- internal/service/order_service_test.go
+++ internal/service/order_service_test.go
@@ -23,30 +23,17 @@ func TestOrderService_Create_Success(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	sqlDB, mock, err := sqlmock.New()
+	gormDB, mock, err := setupDB()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 
-	gormDB, err := gorm.Open(mysql.New(mysql.Config{
-		Conn:                      sqlDB,
-		SkipInitializeWithVersion: true,
-	}), &gorm.Config{})
-
-	validate := validator.New()
+	validate, mockRepo, mockGateway, mockProducer, mockOrderProducer := setupDependencies(ctrl)
 
 	mock.ExpectBegin()
 	mock.ExpectCommit()
 
-	mockRepo := mocks.NewMockOrderRepository(ctrl)
-	mockGateway := mocks.NewMockProductGateway(ctrl)
-	mockProducer := mocks.NewMockProducer[*model.OrderEvent](ctrl)
-	mockOrderProducer := &messaging.OrderProducer{
-		Producer: mockProducer,
-	}
-
 	productId := "f3b2e38a-b709-4e03-bd4d-4d0aa54619a9"
-
 	request := &model.CreateOrderRequest{
 		ProductId: productId,
 	}
@@ -65,7 +52,7 @@ func TestOrderService_Create_Success(t *testing.T) {
 
 	mockProducer.EXPECT().
 		Send("order.created", gomock.Any()).
-		Times(1)
+		Times(1) // The producer should emit order.created when creating new order
 
 	orderService := service.NewOrderService(gormDB, validate, mockRepo, mockOrderProducer, mockGateway)
 
@@ -77,4 +64,69 @@ func TestOrderService_Create_Success(t *testing.T) {
 	if resp == nil {
 		t.Fatalf("expected response, got nil")
 	}
+}
+
+func TestOrderService_GetByProductId_Success(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	gormDB, mock, err := setupDB()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	validate, mockRepo, mockGateway, _, mockOrderProducer := setupDependencies(ctrl)
+
+	mock.ExpectBegin()
+	mock.ExpectCommit()
+
+	productId := "f3b2e38a-b709-4e03-bd4d-4d0aa54619a9"
+	request := &model.GetOrderByProductIdRequest{
+		Id: productId,
+	}
+
+	mockRepo.EXPECT().
+		FindByProductId(
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Any(),
+			gomock.Eq(productId), // Should match with the request
+		).
+		Return(nil)
+
+	orderService := service.NewOrderService(gormDB, validate, mockRepo, mockOrderProducer, mockGateway)
+
+	resp, err := orderService.GetByProductId(context.Background(), request)
+
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+	if resp == nil {
+		t.Fatalf("expected response, got nil")
+	}
+}
+
+func setupDB() (*gorm.DB, sqlmock.Sqlmock, error) {
+	sqlDB, mock, err := sqlmock.New()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	gormDB, err := gorm.Open(mysql.New(mysql.Config{
+		Conn:                      sqlDB,
+		SkipInitializeWithVersion: true,
+	}), &gorm.Config{})
+
+	return gormDB, mock, err
+}
+
+func setupDependencies(ctrl *gomock.Controller) (*validator.Validate, *mocks.MockOrderRepository, *mocks.MockProductGateway, *mocks.MockProducer[*model.OrderEvent], *messaging.OrderProducer) {
+	validate := validator.New()
+	mockRepo := mocks.NewMockOrderRepository(ctrl)
+	mockGateway := mocks.NewMockProductGateway(ctrl)
+	mockProducer := mocks.NewMockProducer[*model.OrderEvent](ctrl)
+	mockOrderProducer := &messaging.OrderProducer{
+		Producer: mockProducer,
+	}
+	return validate, mockRepo, mockGateway, mockProducer, mockOrderProducer
 }
